5665fd9e782a5cc4e137d1cd1c4676eb1615d45d
Rediet Ayalew <rediet.ayalew@drake.edu>
2025-12-12 20:34:10 -0600

    Fixed CORS to allow netlify access the backend

--- backend/server.js
+++ backend/server.js
@@ -8,12 +8,23 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://demo.netlify.app'
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+    cookie: {
+    secure: true,        // required for HTTPS (Netlify)
+    sameSite: 'none'     // required for cross-site cookies
+  }
 }));
 
 // MongoDB Connection
